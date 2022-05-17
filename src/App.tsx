768c52c463a443824c65c379fe972376f5dd847c
KrzysztofKrych <krzysztofkrych2@gmail.com>
2022-05-17 16:38:33 +0200

    add react-draggable package and test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Draggable from 'react-draggable'
+import React from 'react'
 import { TestStyledDiv } from './styles/styled-components'
 
 function App() {
+  const nodeRef = React.useRef(null)
   return (
     <div className='App'>
-      <TestStyledDiv>12</TestStyledDiv>
+      <Draggable nodeRef={nodeRef}>
+        <TestStyledDiv ref={nodeRef}>12</TestStyledDiv>
+      </Draggable>
     </div>
   )
 }
